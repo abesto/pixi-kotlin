b0410229e00b6cb19ff870bd0160a1d8b44428d5
Zoltan Nagy <abesto@abesto.net>
2015-02-06 19:35:16 +0100

    Update Texture and RenderTexture interfaces

--- pixi-kotlin/src/main/kotlin/net/abesto/kotlin/js/pixi/textures/RenderTexture.kt
+++ pixi-kotlin/src/main/kotlin/net/abesto/kotlin/js/pixi/textures/RenderTexture.kt
@@ -1,9 +1,94 @@
 package net.abesto.kotlin.js.pixi.textures
 
 import net.abesto.kotlin.js.pixi.display.Stage
+import net.abesto.kotlin.js.pixi.renderers.Renderer
+import net.abesto.kotlin.js.pixi.PIXI
+import net.abesto.kotlin.js.pixi.geom.Rectangle
+import kotlin.js.dom.html.Image
+import kotlin.js.dom.html5.HTMLCanvasElement
 
 native("PIXI.RenderTexture")
-public class RenderTexture(width: Int, height: Int) : Texture(noImpl, noImpl) {
-    // TODO
+/**
+ * A RenderTexture is a special texture that allows any Pixi display object to be rendered to it.
+ *
+ * __Hint__: All DisplayObjects (i.e. Sprites) that render to a RenderTexture should be preloaded otherwise black rectangles will be drawn instead.
+ *
+ * A RenderTexture takes a snapshot of any Display Object given to its render method. The position and rotation of the given Display Objects is ignored. For example:
+ *
+ *    var renderTexture = PIXI.RenderTexture(800, 600);
+ *    var sprite = PIXI.Sprite.fromImage("spinObj_01.png");
+ *    sprite.position.x = 800/2;
+ *    sprite.position.y = 600/2;
+ *    sprite.anchor.x = 0.5;
+ *    sprite.anchor.y = 0.5;
+ *    renderTexture.render(sprite);
+ *
+ * The Sprite in this case will be rendered to a position of 0,0. To render this sprite at its actual position a DisplayObjectContainer should be used:
+ *
+ *    var doc = PIXI.DisplayObjectContainer();
+ *    doc.addChild(sprite);
+ *    renderTexture.render(doc);  // Renders to center of renderTexture
+ *
+ *
+ */
+public class RenderTexture(
+    /** The width of the render texture */
+    public override var width: Int = 100,
+    
+    /** The height of the render texture */
+    public override var height: Int = 100,
+    
+    /** The renderer used for this RenderTexture. A RenderTexture can only belong to one renderer at the moment if its webGL. */
+    public var renderer: Renderer = noImpl,
+    
+    /** See {{#crossLink "PIXI/scaleModes:property"}}PIXI.scaleModes{{/crossLink}} for possible values */
+    scaleMode: Int = PIXI.scaleModes.DEFAULT,
+    
+    /** The resolution of the texture being generated */
+    public var resolution: Float = 1f
+) : Texture(noImpl, noImpl) {
+    /** The framing rectangle of the render texture */
+    public override var frame: Rectangle = noImpl
+
+    /**
+     * This is the area of the BaseTexture image to actually copy to the Canvas / WebGL when rendering,
+     * irrespective of the actual frame size or placement (which can be influenced by trimmed texture atlases)
+     */
+    public override var crop: Rectangle = noImpl
+    
+    /** The base texture object that this texture uses */
+    public override var baseTexture: BaseTexture = noImpl
+
+    /**
+     * Resizes the RenderTexture.
+     * 
+     * @param width The width to resize to.
+     * @param height The height to resize to.
+     * @param updateBase Should the baseTexture.width and height values be resized as well?
+     */
+    public fun resize(width: Int, height: Int, updateBase: Boolean): Unit = noImpl
+
+    /**
+     * Clears the RenderTexture.
+     */
+    public fun clear(): Unit = noImpl
+
+    /**
+     * Will return a HTML Image of the texture
+     */
+    public fun getImage(): Image = noImpl
+
+    /**
+     * Will return a a base64 encoded string of this texture. It works by calling RenderTexture.getCanvas and then running toDataURL on that.
+     */
+    public fun getBase64(): String = noImpl
+
+    /**
+     * Creates a Canvas element, renders this RenderTexture to it and then returns it.
+     * 
+     * @return A Canvas element with the texture rendered on.
+     */
+    public fun getCanvas(): HTMLCanvasElement = noImpl
+    
     public fun render(stage: Stage, clear: Boolean): Unit = noImpl
 }
--- pixi-kotlin/src/main/kotlin/net/abesto/kotlin/js/pixi/textures/Texture.kt
+++ pixi-kotlin/src/main/kotlin/net/abesto/kotlin/js/pixi/textures/Texture.kt
@@ -1,12 +1,100 @@
 package net.abesto.kotlin.js.pixi.textures
 
 import net.abesto.kotlin.js.pixi.geom.Rectangle
+import net.abesto.kotlin.js.pixi.utils.EventTarget
+import kotlin.js.dom.html5.HTMLCanvasElement
 
 native("PIXI.Texture")
-open public class Texture(baseTexture: BaseTexture, frame: Rectangle) {
-    // TODO
+/**
+ * A texture stores the information that represents an image or part of an image. It cannot be added
+ * to the display list directly. Instead use it as the texture for a PIXI.Sprite. If no frame is provided then the whole image is used.
+ */
+open public class Texture(
+    /** The base texture source to create the texture from */
+    open public var baseTexture: BaseTexture,
+    
+    /** The rectangle frame of the texture to show */
+    open public var frame: Rectangle,
+    
+    /** The area of original texture */
+    open public var crop: Rectangle = noImpl,
+
+    /** Trimmed texture rectangle */
+    public var trim: Rectangle = noImpl
+) : EventTarget() {
+    
+    /** Does this Texture have any frame data assigned to it? */
+    public var noFrame: Boolean = noImpl
+    
+    /** This will let the renderer know if the texture is valid. If it's not then it cannot be rendered. */
+    public var valid: Boolean = noImpl
+
+    /** This will let a renderer know that a texture has been updated (used mainly for webGL uv updates) */
+    public var requiresUpdate: Boolean = noImpl
+    
+    /** The width of the Texture in pixels. */
+    open public var width: Int = noImpl
+    
+    /** The height of the Texture in pixels. */
+    open public var height: Int = noImpl
+
+    /**
+     * Destroys this texture
+     * 
+     * @param destroyBase Whether to destroy the base texture as well
+     */
+    public fun destroy(destroyBase: Boolean): Unit = noImpl
+
+    /**
+     * Specifies the region of the baseTexture that this texture will use.
+     * 
+     * @param frame The frame of the texture to set it to
+     */
+    public fun setFrame(frame: Rectangle): Unit = noImpl
+    
     class object {
-        public fun fromImage(path: String): Texture = noImpl
+        /**
+         * Helper function that creates a Texture object from the given image url.
+         * If the image is not in the texture cache it will be  created and loaded.
+         * 
+         * @param imageUrl The image url of the texture
+         * @param crossorigin  Whether requests should be treated as crossorigin
+         * @param scaleMode See {{#crossLink "PIXI/scaleModes:property"}}PIXI.scaleModes{{/crossLink}} for possible values
+         */
+        public fun fromImage(imageUrl: String, crossorigin: Boolean, scaleMode: Int): Texture = noImpl
+
+        /**
+         * Helper function that returns a Texture objected based on the given frame id.
+         * If the frame id is not in the texture cache an error will be thrown.
+         *
+         * @param frameId The frame id of the texture
+         */
         public fun fromFrame(frameId: String): Texture = noImpl
+
+        /**
+         * Helper function that creates a new a Texture based on the given canvas element.
+         *
+         * @param canvas The canvas element source of the texture
+         * @param scaleMode See {{#crossLink "PIXI/scaleModes:property"}}PIXI.scaleModes{{/crossLink}} for possible values
+         */
+        public fun fromCanvas(canvas: HTMLCanvasElement, scaleMode: Int): Texture = noImpl
+
+        /**
+         * Adds a texture to the global PIXI.TextureCache. This cache is shared across the whole PIXI object.
+         *
+         * @param texture The Texture to add to the cache.
+         * @param id The id that the texture will be stored against.
+         */
+        public fun addTextureToCache(texture: Texture, id: String): Unit = noImpl
+
+        /**
+         * Remove a texture from the global PIXI.TextureCache.
+         *
+         * @param id The id of the texture to be removed
+         * @return The texture that was removed
+         */
+        public fun removeTextureFromCache(id: String): Texture = noImpl
+        
+        public val emptyTexture: Texture = noImpl
     }
 }
